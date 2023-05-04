d6ca6a2afcdf9397ddc45e30ba7de2fb7dce1c98
Isaac Francisco <isaacfrancisco305@gmail.com>
2023-05-03 21:50:09 -0400

    created home screen along with getters and setters

--- src/main/java/org/yup/accountingledger/HomeScreenApp.java
+++ src/main/java/org/yup/accountingledger/HomeScreenApp.java
@@ -1,8 +1,12 @@
 package org.yup.accountingledger;
 
-public class AccountingLedgerApp {
+public class HomeScreen {
 
     public static void main(String[] args){
-        System.out.println(1);
+
+
+        
     }
+
+
 }
